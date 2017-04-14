883d0ce1468236951707f89afd3039035ff4297b
zoro <zonglu@leju.com>
2017-04-14 13:35:04 +0800

    修改

--- application/library/db/Pdo.php
+++ application/library/db/Pdo.php
@@ -4,47 +4,87 @@
  */
 class Db_Pdo{
 
+    /**
+     * 数据库配置信息
+     * @var array
+     */
     private $config;
+
+    /**
+     * pdo连接串
+     * @var string
+     */
     private $dsn;
+
+    /**
+     * PDO实例
+     * @var PDO
+     */
     private $pdo;
+
+    /**
+     * 连接的库名
+     * @var string
+     */
     private $dbname;
 
-    private $dbnum;
+    /**
+     * 主从
+     * @var int 1主库 0从库
+     */
     private $master;
+
+    /**
+     * 事务层
+     * @var integer
+     */
     private $transactionLevel = 0;
 
     private static $instance = [];
 
-    private function __construct($dbnum, $master){
+    private function __construct($dbname, $master){
         $this->master = $master;
-        $this->dbnum = $dbnum;
-        $this->initConfig();
-        $this->initConnectDsn($master);
+        $this->dbname = $dbname;
+        $this->initConfig($master);
+        $this->initConnectDsn();
         $this->initPdo();
     }
 
     private function __clone(){}
 
-    public static function getInstance($dbnum, $master = 1){
-        $key = $dbnum.'_'.$master;
+    public static function getInstance($dbname, $master = 1){
+        $key = $dbname.'_'.$master;
         if(isset(self::$instance[$key]) && !empty(self::$instance[$key])){
             return self::$instance[$key];
         }
-        self::$instance[$key] = new self($dbnum, $master);
+        self::$instance[$key] = new self($dbname, $master);
         return self::$instance[$key];
     }
 
-    public function 
-
-    private function initConfig($dbnum,$master=0){
-        $db_config = Yaf_Registry::get('config')->database->$dbnum->toArray();
+    /**
+     * 初始化配置信息
+     * @param  String  $dbname  数据库配置
+     * @param  integer $master 主库
+     */
+    private function initConfig($master=0){
+        $db_config = Yaf_Registry::get('config')->database->toArray();
         $this->config = $master ? $db_config['master'] : $db_config['slave'];
     }
 
-    private function initConnectDsn($master){
+    /**
+     * 初始化连接dsn
+     * @param  integer $master 主库1 从库0
+     */
+    private function initConnectDsn(){
         $this->dsn = "mysql:host={$this->config['host']};port={$this->config['port']};dbname={$this->dbname}";
+        if(isset($this->config['charset']) && !empty($this->config['charset'])){
+            $this->dsn .= ";charset={$this->config['charset']}";
+        }
     }
 
+    /**
+     * 初始化PDO实例
+     */
     private function initPdo(){
         try{
             $options = array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
@@ -52,7 +92,6 @@ class Db_Pdo{
         }catch (PDOException $e) {
             throw new Exception($e->getMessage());
         }
-        
     }
 
     public function getPdo(){
